5303b4a2284bfa27d56dbb058fd956841a4d40a7
wilsonbotweyo <wilsonotweyo96@gmail.com>
2023-07-06 22:44:42 +0000

    quirk 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set the public folder as the static directory
 app.use(express.static(path.join(__dirname, 'public')));
